a973845011d126b903adf0400469eb6d3b3688ad
Rowan Marshall <rowanajmarshall@gmail.com>
2017-03-02 00:12:47 +0000

    Extended pitch tracking algorithm

--- ocgen/ocgen.py
+++ ocgen/ocgen.py
@@ -27,11 +27,19 @@ def in_bounds(avg_val, num):
 def smooth_pitches(pitches: list, time: int) -> list:
     tolerance = 2
     naive_notes = []
-    avg_val = pitches[0]
-    
+    avg_val = None
+    max_notes = 3 / len(pitches)
+    min_size = len(pitches) / max_notes
+    current_size = 0
     for num in pitches:
+        if avg_val == None:
+            avg_val = num
         if in_bounds(avg_val, num):
             avg_val = int((num + avg_val)/2)
+            current_size += 1
+        elif current_size >= min_size:
+            naive_notes.append(avg_val)
+            avg_val = num
         else:
             naive_notes.append(avg_val)
             avg_val = num

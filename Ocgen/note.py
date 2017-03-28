f753dc6c0c51dd502ebccddc888b4962382803c2
Rowan Marshall <rowanajmarshall@gmail.com>
2017-03-28 11:12:56 +0100

    Fixed find_closest_note

--- Ocgen/note.py
+++ Ocgen/note.py
@@ -13,10 +13,12 @@ class Note:
         self.pitch_num = pitch_num
 
 
+# Throw in the current instrument is out of range
 class NotEnoughRangeError(Exception):
     pass
 
 
+# Representation of the range of a 12-hole ocarina or arbitrary tuning
 def get_12_hole_notes():
     # [(Note, Frequency, Integer representation)]
     return [["A", 440, 1], ["A\#", 466.16, 2], ["B", 493.88, 3], ["C", 523.25, 4], ["C\#", 554.37, 5], ["D", 587.33, 6],
@@ -25,28 +27,33 @@ def get_12_hole_notes():
            ["C\#", 1108.73, 17], ["D", 1174.66, 18], ["D\#", 1244.51, 19], ["E", 1318.51, 20], ["F", 1396.91, 21]]
 
 
-def get_shift(pitches, shift_num=0, ref_list=[i[1] for i in get_12_hole_notes()]):
+# Gets the octave shift required, if any
+def get_shift(pitches: list, shift_num=0, ref_list=[i[1] for i in get_12_hole_notes()]):
     # Lowest current frequency
     lowest = min(shift(ref_list))
 
     # Highest current frequency
     highest = max(shift(ref_list))
 
-    minimum_freq = min(k for k in pitches)
-    maximum_freq = max(k for k in pitches)
+    minimum_freq = min(pitches)
+    maximum_freq = max(pitches)
 
     too_low = minimum_freq < lowest
     too_high = maximum_freq > highest
 
+    # If the pitches cannot be represented with the current instrument setting
     if (too_low and too_high) or (shift_num < 0 and too_high) or (shift_num > 0 and too_low):
         raise NotEnoughRangeError
+    # There are 1 or more pitches lower than the current octave setting
     elif too_low:
         return get_shift(pitches, shift_num - 1, shift(ref_list, -1))
+    # There are 1 or more pitches higher than the current octave setting
     elif too_high:
         return get_shift(pitches, shift_num + 1, shift(ref_list, 1))
     return shift_num
 
 
+# Shift the given list by shift_num ocataves
 def shift(lst, shift_num=0):
     if shift_num < 0:
         return downshift(lst, shift_num)
@@ -55,11 +62,12 @@ def shift(lst, shift_num=0):
     return lst
 
 
-def find_closest_note1(note):
-    return min(get_12_hole_notes(), key=lambda x: abs(float(x[1]) - note))
-
-
 def find_closest_note(note: float, ref_notes, shift_amount=0):
+    ref_notes = shift(ref_notes, shift_amount)
+    return min(ref_notes, key=lambda x: abs(float(x[1]) - note))
+
+
+def alt_find_closest_note1(note: float, ref_notes, shift_amount=0):
 
     ref_notes = shift(ref_notes, shift_amount)
 
@@ -71,10 +79,11 @@ def find_closest_note(note: float, ref_notes, shift_amount=0):
             prev = n
             continue
         elif note < n[1]:
-            return prev
+            return n
     return prev
 
 
+# Ocatave-shift pitches up
 def upshift(note_list: list, shift_num):
     note_list = copy.deepcopy(note_list)
     upshift_factor = 2 ** shift_num
@@ -93,6 +102,7 @@ def upshift(note_list: list, shift_num):
     # return note_list
 
 
+# Ocatave-shift pitches down
 def downshift(note_list: list, shift_num):
     note_list = copy.deepcopy(note_list)
     downshift_factor = 2 ** abs(shift_num)

575c8807e776462a9d8a2d76d10b0d2c7deb0105
Rowan Marshall <rowanajmarshall@gmail.com>
2017-04-02 23:35:40 +0100

    Extra Aloe tests added

--- features/__init__.py
+++ features/__init__.py
@@ -3,15 +3,16 @@ from selenium import webdriver
 import os
 
 driver = webdriver.Firefox()
-driver.get('http://localhost:5000/')
+# driver.get('http://localhost:5000/')
 
-# Make sure animation is hidden
-animation = driver.find_element_by_id('loading')
-assert "display: none" in animation.get_attribute('style')
+@before.each_example
+def navigate(*args):
+    driver.get('http://localhost:5000/index.html')
 
 
 @step('I have selected the file (\S+)')
 def i_have_selected_the_file(step, filename):
+    driver.find_element_by_id("advanced-settings").click()
     file_select_elem = driver.find_element_by_id('choose-file')
     path = os.getcwd() + '/music/' + filename
     path = "~/Diss/ocgen/app/music/" + filename
@@ -26,5 +27,12 @@ def i_click_on_submit(step, button):
 @step('I see the loading animation appear')
 def i_see_the_loading_animation_appear(step):
     driver.implicitly_wait(2)
-    # Make sure animamtion is visible
+    # Make sure animation is visible
     assert "" == driver.find_element_by_id('loading').get_attribute('style')
+
+
+@step('I see the tabs appear after (\d+) seconds')
+def i_see_the_tabs_appear(step, seconds):
+    driver.implicitly_wait(seconds)
+    assert driver.find_element_by_id("result") is not None
+

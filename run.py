9e72cbe079b667e955749e59d74dbcbf9ac38638
Rowan Marshall <rowanajmarshall@gmail.com>
2017-04-02 23:35:02 +0100

    Added support for algorithm selection

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import sys
 from flask import Flask, render_template, request, redirect, flash, url_for
 import os
 
@@ -5,14 +6,13 @@ from flask import send_file
 
 from Ocgen import ocgen
 
-
 app = Flask(__name__)
 app.secret_key = "42"
 app.config['DEBUG'] = True
 
 
-@app.route('/', methods=["GET"])
-def index(filename=None):
+@app.route('/index.html')
+def index():
     return render_template("index.html")
 
 
@@ -20,32 +20,32 @@ def index(filename=None):
 def upload_file():
     UPLOAD_FOLDER = './uploads'
     app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
     start = 0
-    end = -1
+    end = sys.maxsize
+    pitch_algorithm = "yin"
 
     if "upload" not in request.files:
         print("File not in there")
         flash("No file part")
         return redirect(request.url)
 
+    if request.form['start']: start = int(request.form['start'])
+    if request.form['end']: end = int(request.form['end'])
+    if request.form['pitch-algorithm']: pitch_algorithm = request.form['pitch-algorithm']
+
     file = request.files['upload']
-    start = request.form['start']
-    end = request.form['end']
-    print("Start " + str(start))
-    print("End " + str(end))
-    print(request.form)
-    filename = file.filename
 
     if file:
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        result = ocgen.main(os.path.join(app.config['UPLOAD_FOLDER'], filename), start, end, request.form['ocarina-holes'])
-        print(str(result))
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+        result = ocgen.main(os.path.join(app.config['UPLOAD_FOLDER'], file.filename), start, end,
+                            request.form['instrument'], pitch_algorithm)
         return get_result(result)
     return index()
 
 
 def get_result(result):
-    if result is None: return send_file('/home/roan/Diss/ocgen/app/static/result.png')
-    else: return render_template('index.html', error=result)
-
-
+    if result is None:
+        return render_template('result.html')  # return send_file('/home/roan/Diss/ocgen/app/static/result.png')
+    else:
+        return render_template('index.html', error=result)

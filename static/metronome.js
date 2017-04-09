da815b761736622ca62fec722d75fd67351fbc8a
Rowan Marshall <rowanajmarshall@gmail.com>
2017-04-10 00:25:26 +0100

    Formatting

--- static/metronome.js
+++ static/metronome.js
@@ -1,16 +1,23 @@
 
 var metronome = null;
 function playSound() {
+    // File sourced from http://soundbible.com/2044-Tick.html under the MIT Attribution 3.0 licence
+    // https://creativecommons.org/licenses/by/3.0/
     var tick = new Audio('static/tick.mp3');
     tick.play();
 }
 
 function startMetronome() {
-    var interval = ($("#bpm").val() / 60) * 1000;
+   $("#stop").prop('disabled', false)
+    $("#start").prop('disabled', true)
+   var interval = 1000 * 60 / $("#bpm").val();
+    console.log(interval)
     metronome = setInterval(function() { playSound() }, interval);
 }
 
 function stopMetronome() {
+    $("#stop").prop('disabled', true)
+    $("#start").prop('disabled', false)
     clearInterval(metronome);
     metronome = null;
 }
